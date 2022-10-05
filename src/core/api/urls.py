a62c2050eb66d9ea401e4d9b8851b7ac37232172
Alin Floare <alin.floare@cloudpro-systems.com>
2022-10-05 13:37:35 +0300

    removed /

--- src/core/api/urls.py
+++ src/core/api/urls.py
@@ -7,20 +7,20 @@ from core.api.views import (
 )
 
 urlpatterns = [
-    path("alerts/", AlertApiView.as_view(), name="alerts"),
-    path("sites/", SiteApiView.as_view(), name="sites"),
-    path("transaction-history/", TransactionHistoryApiView.as_view(), name="transaction-history"),
+    path("alerts", AlertApiView.as_view(), name="alerts"),
+    path("sites", SiteApiView.as_view(), name="sites"),
+    path("transaction-history", TransactionHistoryApiView.as_view(), name="transaction-history"),
 
     # Operations
-    path("operations/cards-data/", OperationsCardsDataApiView.as_view(), name="operations-cards-data"),
-    path("operations/sites-monitored/", OperationsSiteMonitoredApiView.as_view(), name="operations-cards-data"),
-    path("operations/profile-chart/", OperationsProfileChartApiView.as_view(), name="operations-profile-chart"),
-    path("operations/power-consumption-chart/", OperationsPowerConsumptionChartApiView.as_view(), name="operations-power-chart"),
+    path("operations/cards-data", OperationsCardsDataApiView.as_view(), name="operations-cards-data"),
+    path("operations/sites-monitored", OperationsSiteMonitoredApiView.as_view(), name="operations-cards-data"),
+    path("operations/profile-chart", OperationsProfileChartApiView.as_view(), name="operations-profile-chart"),
+    path("operations/power-consumption-chart", OperationsPowerConsumptionChartApiView.as_view(), name="operations-power-chart"),
 
-    path("operations-dashboard/revenue-loss/", OperationsDashboardRevenueLossApiView.as_view(), name="opd-revenue-loss"),
-    path("operations-dashboard/key-insights/", OperationsDashboardKeyInsightsApiView.as_view(), name="opd-key-insights"),
-    path("operations-dashboard/energy-chart/", OperationsDashboardEnergyChartApiView.as_view(), name="opd-energy-chart"),
-    path("operations-dashboard/cards-data/", OperationsDashboardCardsDataApiView.as_view(), name="opd-cards-data"),
-    path("operations-dashboard/dt-status/", OperationsDashboardDTStatusApiView.as_view(), name="opd-dt-status"),
+    path("operations-dashboard/revenue-loss", OperationsDashboardRevenueLossApiView.as_view(), name="opd-revenue-loss"),
+    path("operations-dashboard/key-insights", OperationsDashboardKeyInsightsApiView.as_view(), name="opd-key-insights"),
+    path("operations-dashboard/energy-chart", OperationsDashboardEnergyChartApiView.as_view(), name="opd-energy-chart"),
+    path("operations-dashboard/cards-data", OperationsDashboardCardsDataApiView.as_view(), name="opd-cards-data"),
+    path("operations-dashboard/dt-status", OperationsDashboardDTStatusApiView.as_view(), name="opd-dt-status"),
 
 ]
