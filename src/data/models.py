1e99d03d22f2256be94917561bf91da32ee34d4c
Alin Floare <alin.floare@cloudpro-systems.com>
2022-10-06 17:55:17 +0300

    Added smart readings model

--- src/data/models.py
+++ src/data/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class SmartDeviceReadings(models.Model):
+    id = models.BigIntegerField(blank=False, null=False, primary_key=True)
+    date = models.TextField(blank=True, null=True)
+    timestamp = models.TextField(blank=True, null=True)
+    gateway_name = models.TextField(blank=True, null=True)
+    gateway_model = models.TextField(blank=True, null=True)
+    gateway_serial = models.TextField(blank=True, null=True)
+    device_name = models.TextField(blank=True, null=True)
+    device_serial = models.TextField(blank=True, null=True)
+    line_to_neutral_voltage_phase_a = models.FloatField(blank=True, null=True)
+    line_to_neutral_voltage_phase_b = models.FloatField(blank=True, null=True)
+    line_to_neutral_voltage_phase_c = models.FloatField(blank=True, null=True)
+    import_active_energy_overall_total = models.FloatField(blank=True, null=True)
+    active_power_overall_total = models.FloatField(blank=True, null=True)
+    analog_input_channel_1 = models.FloatField(blank=True, null=True)
+    analog_input_channel_2 = models.FloatField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'smart_device_readings'
