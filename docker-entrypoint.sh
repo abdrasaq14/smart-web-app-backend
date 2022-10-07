5d473c0d7de1e70cb24a50dd4fba638030c520b5
Hasnain Ali <hasnain96ali@gmail.com>
2022-10-07 09:04:43 +0100

    add mock to entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,5 +3,8 @@
 echo running migrations
 python src/manage.py migrate
 
+echo mocking
+python src/manage.py mock_data --clear True --number 60
+
 echo running server
 python src/manage.py runserver 0.0.0.0:8000
