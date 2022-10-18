8e9b4e19f65cb50e8562abf41027e711c5907b23
Alin Floare <alin.floare@cloudpro-systems.com>
2022-10-18 18:28:55 +0300

    Company serializer. extended other serializers

--- src/core/api/serializers.py
+++ src/core/api/serializers.py
@@ -1,21 +1,34 @@
 from rest_framework import serializers
 
-from core.models import ActivityLog, Alert, EventLog, Site, TransactionHistory, UserLog
+from core.models import ActivityLog, Alert, Company, EventLog, Site, TransactionHistory, UserLog
+
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = ["id", "name", "company_type", "service_type", "phone_number", "email", "address",
+                  "renewal_date", "users"]
+
+
+class SiteSerializer(serializers.ModelSerializer):
+    company = CompanySerializer(many=False)
+
+    class Meta:
+        model = Site
+        fields = ["id", "name", "asset_name", "asset_type", "asset_co_ordinate", "asset_capacity",
+                  "under_maintenance", "is_active", "time", "company"]
 
 
 class ActivityLogSerializer(serializers.ModelSerializer):
+    site = serializers.SlugRelatedField(
+        read_only=True,
+        many=False,
+        slug_field='name'
+    )
+
     class Meta:
         model = ActivityLog
-        fields = [
-            "id",
-            "alert_id",
-            "site",
-            "zone",
-            "district",
-            "activity",
-            "status",
-            "time",
-        ]
+        fields = ["id", "alert_id", "site", "zone", "district", "activity", "status", "time"]
 
 
 class AlertSerializer(ActivityLogSerializer):
@@ -41,34 +54,15 @@ class UserLogSerializer(ActivityLogSerializer):
 class TransactionHistorySerializer(serializers.ModelSerializer):
 
     days = serializers.SerializerMethodField()
+    site = serializers.SlugRelatedField(
+        read_only=True,
+        many=False,
+        slug_field='name'
+    )
 
     class Meta:
         model = TransactionHistory
-        fields = [
-            "id",
-            "site",
-            "subscription",
-            "amount_billed",
-            "amount_bought",
-            "days",
-            "time",
-        ]
+        fields = ["id", "site", "subscription", "amount_billed", "amount_bought", "days", "time"]
 
     def get_days(self, obj):
         return obj.duration_days or 0
-
-
-class SiteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Site
-        fields = [
-            "id",
-            "name",
-            "asset_name",
-            "asset_type",
-            "asset_co_ordinate",
-            "asset_capacity",
-            "under_maintenance",
-            "is_active",
-            "time",
-        ]
