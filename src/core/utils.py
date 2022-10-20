fb6c74076308aeb95bd835f6adf96af620245335
Alin Floare <alin.floare@cloudpro-systems.com>
2022-10-20 11:27:25 +0300

    Extended site filter mixin

--- src/core/utils.py
+++ src/core/utils.py
@@ -1,10 +1,10 @@
 from typing import List
 
 from core.exceptions import GenericErrorException
-from core.models import Site
+from core.models import Company, Site
 
 
-class GetSitesMixin:
+class CompanySiteFiltersMixin:
     def get_sites(self, request) -> List[Site]:
         sites = request.query_params.get("sites", "")
 
@@ -18,6 +18,19 @@ class GetSitesMixin:
         site_ids = sites.split(",")
         return self._search_sites(site_ids)
 
+    def get_companies(self, request) -> List[Company]:
+        companies = request.query_params.get("companies", "")
+
+        if not companies:
+            companies = Company.objects.all()
+            if len(companies) < 1:
+                raise GenericErrorException("No devices linked!")
+
+            return companies
+
+        company_id = companies.split(",")
+        return self._search_companies(company_id)
+
     def _search_sites(self, site_ids: List[str]) -> List[Site]:
         sites = []
 
@@ -28,3 +41,14 @@ class GetSitesMixin:
                 raise GenericErrorException(f"Site: {site_id} does not exist")
 
         return sites
+
+    def _search_companies(self, company_ids: List[str]) -> List[Company]:
+        companies = []
+
+        for company_id in company_ids:
+            try:
+                companies.append(Site.objects.get(id=int(company_id)))
+            except Company.DoesNotExist:
+                raise GenericErrorException(f"Company: {company_id} does not exist")
+
+        return companies
