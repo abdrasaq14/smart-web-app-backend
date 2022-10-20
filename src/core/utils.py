ab8aa65c999648831d484a9367b65d3ac720dae7
Alin Floare <alin.floare@cloudpro-systems.com>
2022-10-20 11:46:05 +0300

    Created company queryset mixin

--- src/core/utils.py
+++ src/core/utils.py
@@ -1,7 +1,13 @@
 from typing import List
+from rest_framework.generics import GenericAPIView
+from django.db.models import Q
 
 from core.exceptions import GenericErrorException
 from core.models import Company, Site
+
+
+class GetSitesMixin:
+    ...
 
 
 class CompanySiteFiltersMixin:
@@ -47,8 +53,41 @@ class CompanySiteFiltersMixin:
 
         for company_id in company_ids:
             try:
-                companies.append(Site.objects.get(id=int(company_id)))
+                companies.append(Company.objects.get(id=int(company_id)))
             except Company.DoesNotExist:
                 raise GenericErrorException(f"Company: {company_id} does not exist")
 
         return companies
+
+
+class CompanySiteDateQuerysetMixin(GenericAPIView, CompanySiteFiltersMixin):
+    """"""
+    time_related_field = 'time'
+    site_related_field = 'site'
+    company_related_field = 'site__company'
+
+    def get_queryset(self):
+        q = Q()
+        queryset = super().get_queryset()
+
+        sites = self.get_sites(self.request)
+        companies = self.get_companies(self.request)
+
+        start_date = self.request.query_params.get("start_date", None)
+        end_date = self.request.query_params.get("end_date", None)
+
+        if start_date and end_date:
+            q = Q(**{f'{self.time_related_field}': [start_date, end_date]})
+        elif start_date and end_date is None:
+            q = Q(**{f'{self.time_related_field}__gte': start_date})
+        elif end_date and start_date is None:
+            q = Q(**{f'{self.time_related_field}__lte': end_date})
+
+        if companies:
+            q = q & Q(**{f'{self.company_related_field}__in': companies})
+
+        if sites:
+            q = q & Q(site__in=sites)
+            q = q & Q(**{f'{self.site_related_field}__in': sites})
+
+        return queryset.filter(q)
