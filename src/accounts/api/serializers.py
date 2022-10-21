72b8dba6b2fa2e26a38da115c024ca89637a47e4
Alin Floare <alin.floare@cloudpro-systems.com>
2022-10-21 09:40:39 +0300

    added company

--- src/accounts/api/serializers.py
+++ src/accounts/api/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 
 from accounts.models import User
+from core.api.serializers import CompanySerializer
 
 
 class UserSerializer(serializers.ModelSerializer):
+    companies = CompanySerializer(many=True)
     class Meta:
         model = User
         fields = ('id', 'access_level', 'email', 'first_name', 'last_name', 'companies', 'employee_id', 'time')
