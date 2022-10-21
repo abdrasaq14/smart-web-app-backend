961949a8f0b9f8ec10cbf5b305ac4882b066f1f4
Alin Floare <alin.floare@cloudpro-systems.com>
2022-10-21 11:12:37 +0300

    Update user

--- src/accounts/api/urls.py
+++ src/accounts/api/urls.py
@@ -7,4 +7,5 @@ from accounts.api.views import CurrentUserView, UserApiView
 urlpatterns = [
     path("users/me", CurrentUserView.as_view(), name="users-me"),
     path("users", UserApiView.as_view(), name="users"),
+    path("users/<int:pk>", UserApiView.as_view(), name="users-details"),
 ]
