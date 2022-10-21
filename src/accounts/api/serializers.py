aa91229106d185a96d4f7e912c400e6ad6f5a2be
Alin Floare <alin.floare@cloudpro-systems.com>
2022-10-21 10:20:57 +0300

    Added companies on create user

--- src/accounts/api/serializers.py
+++ src/accounts/api/serializers.py
@@ -5,9 +5,11 @@ from core.api.serializers import CompanySerializer
 
 
 class UserSerializer(serializers.ModelSerializer):
-    companies = CompanySerializer(many=True)
-
     class Meta:
         model = User
         fields = ('id', 'access_level', 'email', 'first_name', 'last_name', 'companies', 'employee_id', 'time',
-                  'phone_number')
+                  'phone_number', 'companies')
+
+
+class ListUserSerializer(UserSerializer):
+    companies = CompanySerializer(many=True)
--- src/accounts/api/views.py
+++ src/accounts/api/views.py
@@ -1,8 +1,10 @@
 from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 from accounts.models import User
 from core.pagination import TablePagination
-from .serializers import UserSerializer
+from .serializers import ListUserSerializer, UserSerializer
 
 
 class CurrentUserView(ListAPIView):
@@ -15,5 +17,13 @@ class CurrentUserView(ListAPIView):
 
 class UserApiView(ListAPIView, CreateAPIView):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = ListUserSerializer
+    action_serializer_class = UserSerializer
     pagination_class = TablePagination
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.action_serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
