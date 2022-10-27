f4feefb3d7d3f92ef9cba274ef46f5f9a10f8619
Alin Floare <alin.floare@cloudpro-systems.com>
2022-10-27 12:49:40 +0300

    Moved jwt to utils

--- src/accounts/utils.py
+++ src/accounts/utils.py
@@ -1,8 +1,10 @@
 import json
 import os
+from django.http import JsonResponse
+import jwt
+from functools import wraps
 
 from django.contrib.auth import authenticate
-import jwt
 import requests
 from guardian.shortcuts import assign_perm, remove_perm
 
@@ -48,3 +50,35 @@ def jwt_decode_token(token):
     api_identifier = os.environ.get('API_IDENTIFIER')
     issuer = 'https://{}/'.format(auth0_domain)
     return jwt.decode(token, public_key, audience=api_identifier, issuer=issuer, algorithms=['RS256'])
+
+
+def get_token_auth_header(request):
+    """Obtains the access token from the Authorization Header
+    """
+    auth = request.META.get("HTTP_AUTHORIZATION", None)
+    parts = auth.split()
+    token = parts[1]
+
+    return token
+
+
+def requires_scope(required_scope):
+    """Determines if the required scope is present in the access token
+    Args:
+        required_scope (str): The scope required to access the resource
+    """
+    def require_scope(f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            token = get_token_auth_header(args[0])
+            decoded = jwt.decode(token, verify=False)
+            if decoded.get("scope"):
+                token_scopes = decoded["scope"].split()
+                for token_scope in token_scopes:
+                    if token_scope == required_scope:
+                        return f(*args, **kwargs)
+            response = JsonResponse({'message': 'You don\'t have access to this resource'})
+            response.status_code = 403
+            return response
+        return decorated
+    return require_scope
