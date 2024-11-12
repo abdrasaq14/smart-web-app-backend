e20c291ccac55c5d0ee33d933caa14d108442aac
oju-31 <ojutomoria@gmail.com>
2024-11-12 16:37:02 +0000

    changed access level to permissions

--- src/core/permissions.py
+++ src/core/permissions.py
@@ -10,14 +10,14 @@ class BaseAccountPermission(BasePermission):
     Simple base account permissions.
     """
 
-    access_types = []
+    required_permissions = []
 
     def requires_scope(self, request, required_scopes: List[str]):
         token = get_token_auth_header(request)
         decoded = jwt.decode(token, verify=False)
 
-        if decoded.get("scope"):
-            token_scopes = decoded["scope"].split()
+        if decoded.get("permissions"):
+            token_scopes = decoded["permissions"].split()
             for token_scope in token_scopes:
                 if token_scope in required_scopes:
                     return True
@@ -26,29 +26,29 @@ class BaseAccountPermission(BasePermission):
 
     def requires_access_level(self, request, required_scopes: List[str]):
         user = request.user
-        if user.access_level in required_scopes:
+        if user.permissions in required_scopes:
             return True
         return False
 
     def has_permission(self, request, view):
-        return self.requires_access_level(request, self.access_types)
+        return self.requires_access_level(request, self.required_permissions)
 
 
 class AdminAccessPermission(BaseAccountPermission):
     """Admin access permissions"""
-    access_types = ['admin']
+    required_permissions = ['admin:access']
 
 
 class ManagerAccessPermission(BaseAccountPermission):
     """Manager access permissions"""
-    access_types = ['admin', 'manager']
+    required_permissions = ['admin:access', 'manager:access']
 
 
 class OperationAccessPermission(BaseAccountPermission):
     """Operation access permissions"""
-    access_types = ['admin', 'manager', 'operation']
+    required_permissions = ['admin:access', 'manager:access', "operation:access"]
 
 
 class FinanceAccessPermission(BaseAccountPermission):
     """Finance access permissions"""
-    access_types = ['admin', 'manager', 'operation', 'finance']
+    required_permissions = ['admin:access', 'manager:access', "operation:access", "finance:access"]
